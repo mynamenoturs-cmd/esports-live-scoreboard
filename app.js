8f5a6f5d45d16031ace7e5944fe8ee22f58498ac
mynamenoturs-cmd <mynamenoturs@gmail.com>
2026-09-15 10:16:37 +0800

    Refresh MLBB live result rendering

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import './series-style-loader.js?v=series1';
+import './series-style-loader.js?v=series3';
+import './mlbb-result-public.js?v=series3';
 import { loadTournamentBundle, subscribeTournament, applyRealtimeChange, filterBundle, gameFromLocation, gameUrl } from './data.js';
-import { liveMatchHtml, standingsHtml, scheduleHtml, teamsHtml, nextMatchHtml, gameTabsHtml, formatSummaryText } from './ui.js?v=series1';
+import { liveMatchHtml, standingsHtml, scheduleHtml, teamsHtml, nextMatchHtml, gameTabsHtml, formatSummaryText } from './ui.js?v=series3';
 
 let sub,root,selectedCode;
 function view(){ return filterBundle(root,selectedCode); }
